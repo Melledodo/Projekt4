d9c451246e6b34a88f52730a0410d92c954329a2
Melledodo <mkaagaard@hotmail.com>
2023-11-07 13:54:13 +0100

    girltalk

--- projekt4.js
+++ projekt4.js
@@ -34,3 +34,47 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block"; 
   setTimeout(showSlides, 3000); // skift billede hvert 3 sek
 }
+
+// Michelle
+
+const buttons = document.querySelectorAll("button.button");
+const modals = document.querySelectorAll("dialog.modal");
+const closes = document.querySelectorAll(".modal__close");
+
+function closeModal(modal) {
+  modal.setAttribute("aria-state", "closed");
+  setTimeout(function () {
+    modal.close();
+  }, 300);
+}
+
+function openModal(modal) {
+  modal.showModal();
+
+  setTimeout(function () {
+    modal.setAttribute("aria-state", "open");
+  }, 300);
+}
+
+buttons.forEach((button, index) => {
+  button.addEventListener("click", function (e) {
+    openModal(modals[index]);
+  });
+});
+
+closes.forEach((close, index) => {
+  close.addEventListener("click", function (e) {
+    closeModal(modals[index]);
+  });
+});
+
+modals.forEach((modal) => {
+  modal.addEventListener("click", function (e) {
+    if (e.target.tagName === "DIALOG") {
+      const index = modals.indexOf(modal);
+      closeModal(modals[index]);
+    }
+  });
+});
+
+//Lisbet
