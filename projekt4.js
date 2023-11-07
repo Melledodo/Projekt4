9f9890b1966e258ad4c90b30fd5d4e104a45d15f
lillianmatti <132983369+lillianmatti@users.noreply.github.com>
2023-11-07 11:59:10 +0100

    banner

--- projekt4.js
+++ projekt4.js
@@ -1,3 +1,4 @@
+// Sarah 
 let scrollContainer = document.querySelector(".galleri");
 let backBtn = document.getElementById("backBtn");
 let nexBtn = document.getElementById("nextBtn");
@@ -16,3 +17,20 @@ backBtn.addEventListener("click", ()=>{
     scrollContainer.style.scrollBehavior = "smooth"; 
     scrollContainer.scrollLeft -= 900;
 }); 
+
+
+// Lillian 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 3000); // skift billede hvert 3 sek
+}
